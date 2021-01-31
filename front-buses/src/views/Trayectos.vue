9a018c4463d94fe3649c6b2c8a7fe65aa1661340
David <davip.rivas@gmail.com>
2021-01-31 14:40:47 -0300

    crud se añade el eliminar

--- front-buses/src/views/Trayectos.vue
+++ front-buses/src/views/Trayectos.vue
@@ -235,12 +235,12 @@
 								<v-dialog v-model="dialogDelete" max-width="500px">
 									<v-card>
 										<v-card-title class="headline"
-											>Are you sure you want to delete this item?</v-card-title
+											>¿ Estas seguro de eliminar este registro ?</v-card-title
 										>
 										<v-card-actions>
 											<v-spacer></v-spacer>
-											<v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-											<v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+											<v-btn color="red darken-1" outlined @click="closeDelete">No</v-btn>
+											<v-btn color="green darken-4" outlined @click="deleteItemConfirm">Si</v-btn>
 											<v-spacer></v-spacer>
 										</v-card-actions>
 									</v-card>
@@ -539,7 +539,6 @@ export default {
 						this.listadoTrayectos.push(trayecto);
 					}
 				});
-				console.log(this.listadoTrayectos);
 			} else {
 				this.muestraMensaje(
 					'error',
@@ -550,25 +549,48 @@ export default {
 			}
 		},
 
-		eliminarRegistros() {},
-
 		deleteItem(item) {
 			this.editedIndex = this.listadoTrayectos.indexOf(item);
-			this.editedItem = Object.assign({}, item);
+			this.objTrayecto = Object.assign({}, item);
 			this.dialogDelete = true;
 		},
 
-		deleteItemConfirm() {
-			this.listadoTrayectos.splice(this.editedIndex, 1);
-			this.closeDelete();
+		async deleteItemConfirm() {
+			try {
+				let peticion = await axios.delete(
+					`http://localhost:8000/api/trayectos/${this.objTrayecto.id}/`
+				);
+
+				if (peticion.status == 204) {
+					this.muestraMensaje(
+						'success',
+						'mdi-check-all',
+						'Estimado Usuario, se ha eliminado el registro',
+						'Correcto'
+					);
+				} else {
+					throw new Error('Error');
+				}
+			} catch (error) {
+				this.muestraMensaje(
+					'error',
+					'mdi-cloud-alert',
+					'Estimado Usuario, se le informa que ha ocurrido un error, favor comunicarse con el administrador',
+					'Error'
+				);
+			} finally {
+				this.closeDelete();
+			}
 		},
 
 		closeDelete() {
 			this.dialogDelete = false;
 			this.$nextTick(() => {
-				this.editedItem = Object.assign({}, this.defaultItem);
+				this.objTrayecto = Object.assign({}, this.defaultItem);
 				this.editedIndex = -1;
 			});
+
+			this.traerTrayectos();
 		},
 
 		async actualizarElemento() {
